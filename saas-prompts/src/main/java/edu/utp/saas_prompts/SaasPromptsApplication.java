57196d35e7057abe5e09e00b86b2e07e8bdc7ad5
Cristopher Vera Quinde <cvequin@hotmail.com>
2026-09-01 02:23:15 -0500

    nuevo spring 2 maven

---
+++ saas-prompts/src/main/java/edu/utp/saas_prompts/SaasPromptsApplication.java
@@ -0,0 +1,13 @@
+package edu.utp.saas_prompts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasPromptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaasPromptsApplication.class, args);
+	}
+
+}
---
+++ saas-prompts/src/test/java/edu/utp/saas_prompts/SaasPromptsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utp.saas_prompts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasPromptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
