7f08eafba3f2bc49dc6da5d836440d1449270eea
Cristopher Vera Quinde <cvequin@hotmail.com>
2026-08-31 22:00:29 -0500

    spring init
    java maven 21

---
+++ saas-deep-research/src/test/java/com/example/saas_deep_research/SaasDeepResearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saas_deep_research;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasDeepResearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
